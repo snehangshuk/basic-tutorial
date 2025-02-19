64235ac891965217a3daafa24cfca2cf3805753c
snehangshuk <snehangshu@gmail.com>
2025-02-19 16:03:15 +0530

    typo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    'sphynx_terminal',
+    'sphinx_termynal',
 ]
 
 intersphinx_mapping = {
